db4f67d926cb92dce58a090942b0f8e85edb0f6b
JandersonCRB <jal@ic.ufal.br>
2023-02-13 00:15:30 -0300

    Adding championship edition validations

--- app/models/championship_edition.rb
+++ app/models/championship_edition.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class ChampionshipEdition < ApplicationRecord
   belongs_to :championship
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: { scope: :championship_id }
+  # TODO: write specs for validations
 end
